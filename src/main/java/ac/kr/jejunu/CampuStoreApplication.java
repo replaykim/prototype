3d2d9ca6752e6d7d1feaf04c0781e09e26e7f46f
hongjinbom <hongjinbom@gmail.com>
2017-04-27 19:41:40 +0900

    initial commit

---
+++ src/main/java/ac/kr/jejunu/CampuStoreApplication.java
@@ -0,0 +1,12 @@
+package ac.kr.jejunu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampuStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampuStoreApplication.class, args);
+	}
+}
---
+++ src/test/java/ac/kr/jejunu/CampuStoreApplicationTests.java
@@ -0,0 +1,16 @@
+package ac.kr.jejunu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CampuStoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

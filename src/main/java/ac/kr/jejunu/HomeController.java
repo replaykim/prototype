e6a582e8ba53687d67ad306b19babae68823aa3b
hongjinbom <hongjinbom@gmail.com>
2017-05-05 06:03:42 +0900

    init

---
+++ src/main/java/ac/kr/jejunu/HomeController.java
@@ -0,0 +1,18 @@
+package ac.kr.jejunu;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by highdali on 2017. 4. 30..
+ */
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String hello() {
+        System.out.println("hello");
+        return "index";
+    }
+}

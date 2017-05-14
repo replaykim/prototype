88a35a34f236c0a96cbb985c81318cd0cc65366b
Eunhyeong Boo <boo1984@naver.com>
2017-05-14 23:20:20 +0900

    Update App.java
    Make private, primitive type

--- src/main/java/ac/kr/jejunu/common/model/App.java
+++ src/main/java/ac/kr/jejunu/common/model/App.java
@@ -15,30 +15,30 @@ import java.util.Date;
 public class App {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    int app_no; // 식별자
+    private Long app_no; // 식별자
 
     String name; // 앱의 이름
 
     @ManyToOne
     @JoinColumn(name = "uploader_no", referencedColumnName = "user_no")
-    User user;
-    String description; // 앱 설명
-    String version; // 앱 버전
-    int volume; // 앱 용량
+    private User user;
+    private String description; // 앱 설명
+    private String version; // 앱 버전
+    private Integer volume; // 앱 용량
 
     @ManyToOne
     @JoinColumn(name = "category_no", referencedColumnName = "category_no")
-    Category category;
+    private Category category;
 
-    String storage; // 앱 저장소 URL
+    private String storage; // 앱 저장소 URL
 
-    int view_count; // 조회수
-    int download_count; // 다운로드 횟수
-    int stared; // 별점수
+    private Integer view_count; // 조회수
+    private Integer download_count; // 다운로드 횟수
+    private Integer stared; // 별점수
 
-    int fund_goal; // 펀딩 목표 금액
-    int fund_amount;// 펀딩된 금액
+    private Integer fund_goal; // 펀딩 목표 금액
+    private Integer fund_amount;// 펀딩된 금액
 
-    Date register_date;// 신규 업로드 일자
-    Date update_date;// 최근 업데이트 일자
+    private Date register_date;// 신규 업로드 일자
+    private Date update_date;// 최근 업데이트 일자
 }
